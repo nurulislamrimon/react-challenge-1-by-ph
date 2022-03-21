241474b37e88a1e42cd9b2abb08da231330c2822
nurulislamrimon <nurulislamrimon@gmail.com>
2022-03-21 22:24:38 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter></Counter>
     </div>
   );
+}
+
+const Counter = () => {
+  const [charge, setCharge] = useState(100);
+  const decreaseCharge = () => setCharge(charge - 10);
+  return (
+    <div className='container'>
+      <h1>Battery: {charge}%</h1>
+      <button onClick={decreaseCharge}>Battery Down</button>
+    </div>
+  )
 }
 
 export default App;
